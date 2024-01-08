415ed94f21b1d57aef54e91078b35a03fe071a67
MartinTande <mrtande95@gmail.com>
2024-01-08 22:04:08 +0100

    Add oppgave 1 of øving uke 2

---
+++ Øving_uke2/src/oppgave1/Heltall.java
@@ -0,0 +1,17 @@
+package oppgave1;
+
+public class Heltall {
+	private int tall;
+	
+	public Heltall(int n) {
+	tall = n;
+	}
+	public void inkrementer() {
+	tall++;
+	}
+	@Override
+	public String toString() {
+	return "" + tall;
+	}
+
+}
